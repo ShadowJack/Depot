d6896293c89a8aafac50567241485765ec73d9d1
ShadowJack <kostyap@hotmail.ru>
2012-06-24 10:08:37 +0400

    validation for product fields

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|png|jpg|jpeg)$}i,
+    :message => 'must be an URL for gif or png or jpg or jpeg image'
+  }
 end
