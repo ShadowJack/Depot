4cab8ff400ba237047cb04444a3e4b42e75a6843
ShadowJack <kostyap@hotmail.ru>
2012-07-20 17:54:06 +0400

    Before russianalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to store_url, :notice => 'Sorry, access denied!'
+  end
+  
   private 
   
     def current_cart
